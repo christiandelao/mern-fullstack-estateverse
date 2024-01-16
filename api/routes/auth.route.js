64dfbb8d4c6d8bb5bd6cce5d8ddcc276de0c745a
Christian Delao <christiandelao10@.gmail.com>
2024-01-15 19:24:36 -0800

    Add update user route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,7 +4,8 @@ import { signup, signin, google } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/signup', signup);
-router.post('/signin', signin)
-router.post('/google', google )
+router.post('/signin', signin);
+router.post('/google', google );
+
 
 export default router;
