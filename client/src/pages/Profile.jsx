039634f035ddf4ee1b7aa7d6d139f29c819a6196
Christian Delao <christiandelao10@.gmail.com>
2024-01-15 21:22:15 -0800

    Added delete account function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useRef, useState, useEffect } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from "../firebase";
-import { updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice.js";
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice.js";
 export default function Profile() {
 const {currentUser, loading, error} = useSelector((state)=>state.user)
 const fileRef = useRef(null);
@@ -66,7 +66,24 @@ const handleSubmit = async (e) =>{
         dispatch(updateUserFailure(error.message))
     }
 }
-return (
+
+const handleDeleteUser = async () =>{
+    try {
+        dispatch(deleteUserStart());
+        const res = await fetch (`/api/user/delete/${currentUser._id}`,{
+            method:'DELETE',
+        });
+        const data = await res.json();
+        if (data.success === false) {
+            dispatch(deleteUserFailure(data.message));
+            return;
+        }
+        dispatch(deleteUserSuccess(data));
+    } catch (error) {
+        dispatch(deleteUserFailure(error.message));
+    }
+}
+return ( 
     <div className="p-3 max-w-lg mx-auto">
         <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
@@ -94,7 +111,7 @@ return (
 
         </form>
         <div className="flex justify-between mt-5">
-            <span className="text-red-700 cursor-pointer">Delete Account</span>
+            <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
             <span className="text-red-700 cursor-pointer">Sign Out</span>
         </div>
         <p className="text-red-700 mt-5 text-center">
