e9b387dc93359be5a9fecb99985d858177bfa591
Christian Delao <christiandelao10@.gmail.com>
2024-01-15 21:33:39 -0800

    Created sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -83,6 +83,21 @@ const handleDeleteUser = async () =>{
         dispatch(deleteUserFailure(error.message));
     }
 }
+
+const handleSignOut = async () =>{
+    try {
+        dispatch(updateUserStart());
+        const res = await fetch ('/api/auth/signout');
+        const data = await res.json();
+        if (data.success === false){
+            dispatch(updateUserFailure(data.message));
+            return;
+        }
+        dispatch(deleteUserSuccess(data));
+    } catch (error) {
+        dispatch(updateUserFailure(error.message))
+    }
+}
 return ( 
     <div className="p-3 max-w-lg mx-auto">
         <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -112,7 +127,7 @@ return (
         </form>
         <div className="flex justify-between mt-5">
             <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-            <span className="text-red-700 cursor-pointer">Sign Out</span>
+            <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
         </div>
         <p className="text-red-700 mt-5 text-center">
             {error ? error: ''}
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -46,6 +46,18 @@ const userSlice = createSlice({
         state.error = action.payload;
         state.loading = false;
     },
+    signOutUserStart: (state) => {
+        state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+    },
 }});
 
 export const {signInFailure, signInStart, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess} = userSlice.actions;
